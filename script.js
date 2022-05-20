cb9f01baadd8911ad2781bfb26e5930751202d0a
Itallo <italloalexsander@gmail.com>
2022-05-20 14:22:31 -0300

    Change to accept custom grid size

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas');
 
 
 const mouseOverHandler = () => {
-    const boxes = document.getElementsByClassName('box16');
+    const boxes = document.getElementsByClassName('boxGrid');
     console.log(boxes);
     Array.from(boxes).forEach(element => {
         element.addEventListener('mouseover', () => {
@@ -13,15 +13,25 @@ const mouseOverHandler = () => {
 
 const selectionHandler = (input) =>{
     //Maybe add a way to calculate the size of the box by dividing the canvas by the input?
+    /*box-sizing: border-box;
+    width: 31.25px;
+    height: 31.25px;
+    border: 1px solid black;  This CSS is here just for reference to the function below*/
+    let sizeOfDivs = (500/input);
+    sizeOfDivs.toFixed(1);
     for(i = 0;i < input*input; i++){
         aux = document.createElement('div');
-        aux.classList.add('box16')
+        aux.style.height = `${sizeOfDivs}px`;
+        aux.style.width = `${sizeOfDivs}px`;
+        aux.style.boxSizing = 'border-box';
+        aux.style.border = '1px solid black';
+        aux.classList.add('boxGrid');
         canvas.appendChild(aux);
     }
     mouseOverHandler();
 }
 
-let grid = 16;
+let grid = 32;
 
 
 selectionHandler(grid);
