9cb38dc11224ae647156eb2038f00534160b9e9b
Itallo <italloalexsander@gmail.com>
2022-05-20 13:47:32 -0300

    Add mouseover eventlistener and the style to match

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const canvas = document.getElementById('canvas');
 
+
+const mouseOverHandler = () => {
+    const boxes = document.getElementsByClassName('box16');
+    console.log(boxes);
+    Array.from(boxes).forEach(element => {
+        element.addEventListener('mouseover', () => {
+            element.classList.add('boxColored');
+        });
+    });
+}
+
 const selectionHandler = (input) =>{
+    //Maybe add a way to calculate the size of the box by dividing the canvas by the input?
     for(i = 0;i < input*input; i++){
         aux = document.createElement('div');
         aux.classList.add('box16')
         canvas.appendChild(aux);
     }
-    
+    mouseOverHandler();
 }
 
-selectionHandler(16);
+let grid = 16;
+
+
+selectionHandler(grid);
